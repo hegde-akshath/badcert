fba1e139edd04350f1c566f596b1ba4a4aa65c3e
hegde-akshath <arhsagar@gmail.com>
2025-11-03 15:38:20 +0530

    updating

--- bin/badcert-generator.go
+++ bin/badcert-generator.go
@@ -13,7 +13,7 @@ var defaultCertificateParams *DefaultCertificateParams
 
 
 func main() {
-	commandType         := flag.String("command", "custom-certs", "Command To run(custom-certs | rfc-5280-certs | sign-request)")
+	commandType         := flag.String("command", "ca-authenticate-certs", "Command To run(ca-authenticate-certs | rfc-5280-certs | sign-request)")
 	certOutputDirectory := flag.String("cert_dir", "certs/", "Certificate output directory path")
 	certRequestType     := flag.Int("cert_request_type", int(LEAF_CERT_VERSION_1), "Type of leaf certificate to create during signing")
 	certRequestPath     := flag.String("cert_request_path", "req.pem", "Path to certificate signing request file")
@@ -29,9 +29,9 @@ func main() {
 	if (*commandType == "rfc-5280-certs") {
 		rfc5280CertOutputDirectory := fmt.Sprintf("%s/rfc-5280-certs/", absPath)
 	        GenerateRFC5280Certs(rfc5280CertOutputDirectory)
-	} else if (*commandType == "custom-certs") {
-	        customCertOutputDirectory := fmt.Sprintf("%s/custom-certs/", absPath)
-	        GenerateCustomCerts(customCertOutputDirectory)
+	} else if (*commandType == "ca-authenticate-certs") {
+	        caAuthenticateCertOutputDirectory := fmt.Sprintf("%s/ca-authenticate-certs/", absPath)
+	        GenerateCAAuthenticateCerts(caAuthenticateCertOutputDirectory)
 	} else if (*commandType == "sign-request") {
 		signRequestCertOutputDirectory := fmt.Sprintf("%s/sign-request/", absPath)
 		SignRequest(*defaultCADirectory, signRequestCertOutputDirectory, CertRequestType(*certRequestType), *certRequestPath, CertRequestSigner(*certRequestSigner))
--- bin/chains.go
+++ bin/chains.go
@@ -79,22 +79,46 @@ func BuildBadCertificateChains(badRootCARecipe *badcert.BadCertificate, badInter
         badIntermed1CARecipe.SignTBS(defaultCertificateParams.RootCAKey, defaultCertificateParams.SignatureAlgorithm)
 	badLeafRecipe.SignTBS(defaultCertificateParams.Intermed1CAKey, defaultCertificateParams.SignatureAlgorithm)
         
-	//This creates a completely valid cert chain. And it will be the same in all cases. Removing it to avoid repetition
-	//certChain1 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, true, true,    goodLeafRecipe, goodIntermed1CARecipe, goodRootCARecipe)
-	certChain1 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, true, false,   badLeafRecipe, goodIntermed1CARecipe, goodRootCARecipe)
-	certChain2 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, true,   goodLeafRecipe, badIntermed1CARecipe, goodRootCARecipe)
-	certChain3 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, false,  badLeafRecipe, badIntermed1CARecipe, goodRootCARecipe)
-	certChain4 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, true, true,   goodLeafRecipe, goodIntermed1CARecipe, badRootCARecipe)
-	certChain5 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, true, false,  badLeafRecipe, goodIntermed1CARecipe, badRootCARecipe)
-	certChain6 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, true,  goodLeafRecipe, badIntermed1CARecipe, badRootCARecipe)
-	certChain7 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, false, badLeafRecipe, badIntermed1CARecipe, badRootCARecipe)
+	//NOTE: The combination with all good certificates won't be generated here, as it will be repetitive regardless of how the function is called
+	
+	//CHAIN 1  = Bad Root CA   -> Good Leaf
+	certChain1 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, true, goodLeafRecipe,badRootCARecipe)
+
+	//CHAIN 2  = Bad Root CA   -> Bad Leaf
+	certChain2 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, false, badLeafRecipe,badRootCARecipe)
+
+	//CHAIN 3  = Good Root CA  -> Bad Leaf
+	certChain3 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, false, badLeafRecipe,goodRootCARecipe)
+	
+	//CHAIN 4  = Good Root CA  -> Good Intermediate CA  -> Bad Leaf
+	certChain4 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, true, false, badLeafRecipe,goodIntermed1CARecipe,goodRootCARecipe)
+	
+	//CHAIN 5  = Good Root CA  -> Bad Intermediate CA   -> Good Leaf
+	certChain5 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, true, goodLeafRecipe,badIntermed1CARecipe,goodRootCARecipe)
+	
+	//CHAIN 6  = Good Root CA  -> Bad Intermediate CA   -> Bad Leaf
+	certChain6 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, false, badLeafRecipe,badIntermed1CARecipe,goodRootCARecipe)
+	
+	//CHAIN 7  = Bad Root CA   -> Good Intermediate CA  -> Good Leaf
+	certChain7 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, true, true, goodLeafRecipe,goodIntermed1CARecipe,badRootCARecipe)
+	
+	//CHAIN 8  = Bad Root CA   -> Good Intermediate CA  -> Bad Leaf
+	certChain8 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, true, false, badLeafRecipe,goodIntermed1CARecipe,badRootCARecipe)
+	
+	//CHAIN 9  = Bad Root CA   -> Bad Intermediate CA   -> Good Leaf
+	certChain9 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, true, goodLeafRecipe,badIntermed1CARecipe,badRootCARecipe)
+	
+	//CHAIN 10 = Bad Root CA   -> Bad Intermediate CA   -> Bad Leaf
+	certChain10 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, false, badLeafRecipe,badIntermed1CARecipe,badRootCARecipe)
 	
 
-	badCertificateChains := CreateBadCertificateChains(certChain1, certChain2, certChain3, certChain4, certChain5, certChain6, certChain7)
+	badCertificateChains := CreateBadCertificateChains(certChain1, certChain2, certChain3, certChain4, certChain5, certChain6, certChain7, certChain8, certChain9, certChain10)
 	return &badCertificateChains
 }
 
-//This is meant to be a quick generator of combinations of  3 level cert chain. The keys used for signing are from default parameters
+//This is meant to be a quick generator of combinations of a 3 level cert chain with a leaf thats always good. 
+//It accepts bad certificates for root and intermed1 CAs. It uses a good leaf certificate generated from default recipe
+//The keys used for signing are from default parameters
 //So it can't be sued in cases where the bad certificate is created due to modification related to key or signature params
 //Or a chain with a different setup
 //In such cases, use the underlying cert generation functions directly
@@ -109,13 +133,22 @@ func BuildBadCACertificateChains(badRootCARecipe *badcert.BadCertificate, badInt
         badRootCARecipe.SignTBS(defaultCertificateParams.RootCAKey, defaultCertificateParams.SignatureAlgorithm)
         badIntermed1CARecipe.SignTBS(defaultCertificateParams.RootCAKey, defaultCertificateParams.SignatureAlgorithm)
         
-	//This creates a completely valid cert chain. And it will be the same in all cases. Removing it to avoid repetition
-	//certChain1 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, true, true,   goodLeafRecipe, goodIntermed1CARecipe, goodRootCARecipe)
-	certChain1 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, true,  goodLeafRecipe, badIntermed1CARecipe, goodRootCARecipe)	
-	certChain2 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, true, true,  goodLeafRecipe, goodIntermed1CARecipe, badRootCARecipe)
-	certChain3 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, true, goodLeafRecipe, badIntermed1CARecipe, badRootCARecipe)
+	//NOTE: The combination with all good certificates won't be generated here, as it will be repetitive regardless of how the function is called
 
-	badCertificateChains := CreateBadCertificateChains(certChain1, certChain2, certChain3)
+	//CHAIN 1 = Bad Root CA  -> Good Leaf
+	certChain1 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, true, goodLeafRecipe,badRootCARecipe)	
+	
+	//CHAIN 2 = Good Root CA -> Bad Intermediate CA  -> Good Leaf
+	certChain2 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, true, false, true, goodLeafRecipe,badIntermed1CARecipe,goodRootCARecipe)	
+	
+	//CHAIN 3 = Bad Root CA  -> Good Intermediate CA -> Good Leaf
+	certChain3 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, true, true, goodLeafRecipe,goodIntermed1CARecipe,badRootCARecipe)
+	
+	//CHAIN 4 = Bad Root CA  -> Bad Intermediate CA  -> Good Leaf
+	certChain4 := CreateBadCertificateChain(certProfileBaseDescription, defaultCertificateParams.LeafKey, false, false, true, goodLeafRecipe,badIntermed1CARecipe,badRootCARecipe)
+
+	
+	badCertificateChains := CreateBadCertificateChains(certChain1, certChain2, certChain3, certChain4)
 	return &badCertificateChains
 }
 
